c9e718b925ba3e06cba69216f9408973b221683b
MBoota <m43577535@gmail.com>
2023-11-09 22:47:51 +0500

    showing all userList

---
+++ Dapper_App/Controllers/PersonController.cs
@@ -0,0 +1,43 @@
+﻿using Dapper_DB.Models.Doman;
+using Dapper_DB.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dapper_App.Controllers
+{
+    public class PersonController : Controller
+    {
+        private readonly IPersonRepository _repo;
+        public PersonController(IPersonRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var list =await _repo.GetListAsync();
+            return View(list);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public IActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //public IActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //public IActionResult Create()
+        //{
+        //    return View();
+        //}
+    }
+}
